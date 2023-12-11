13a953d7a4e94e9397f6918fd99002d07ad6bebf
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-11 16:59:20 -0300

    Ok

--- src/cl/inacap/controller/TrabajadorControlador.java
+++ src/cl/inacap/controller/TrabajadorControlador.java
@@ -31,32 +31,32 @@ public class TrabajadorControlador extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-				//1. Se traen los datos.
-				String nombre = request.getParameter("nombre");
-				String apellido = request.getParameter("apellido");
-				String edad = request.getParameter("edad");
-				String pais = request.getParameter("pais");
-				String tecnologia = request.getParameter("tecnologia");
-				String comentario = request.getParameter("comentario");
-				String experiencia = request.getParameter("experiencia");
-				
-				//2. Se crea el objeto.
-				Trabajador tr = new Trabajador();
-				tr.setNombre(nombre);
-				tr.setApellido(apellido);
-				tr.setEdad(Integer.parseInt(edad));
-				tr.setPais(pais);
-				tr.setTecnologia(tecnologia);
-				tr.setComentario(comentario);
-				tr.setExperiencia(experiencia);
-				
-				//3. Se añade el objeto a la lista.			
-				TrabajadorDAO daoTrabajadores = new TrabajadorDAO();
-				daoTrabajadores.save(tr); //Se añade el objeto a su respectivo Modelo.
-				List<Trabajador> listaTrabajadores = daoTrabajadores.getAll(); //Aca se trae la lista.
-				
-				request.setAttribute("listaTrabajadores", listaTrabajadores);
-				request.getRequestDispatcher("vista_trabajador.jsp").forward(request, response); //Aca se utiliza la interfaz RequestDispatcher para indicar a donde enviar los datos y el método forward para enviar los datos.
+		//1. Se traen los datos.
+		String nombre = request.getParameter("nombre");
+		String apellido = request.getParameter("apellido");
+		String edad = request.getParameter("edad");
+		String pais = request.getParameter("pais");
+		String tecnologia = request.getParameter("tecnologia");
+		String comentario = request.getParameter("comentario");
+		String experiencia = request.getParameter("experiencia");
+		
+		//2. Se crea el objeto.
+		Trabajador tr = new Trabajador();
+		tr.setNombre(nombre);
+		tr.setApellido(apellido);
+		tr.setEdad(Integer.parseInt(edad));
+		tr.setPais(pais);
+		tr.setTecnologia(tecnologia);
+		tr.setComentario(comentario);
+		tr.setExperiencia(experiencia);
+		
+		//3. Se añade el objeto a la lista.			
+		TrabajadorDAO daoTrabajadores = new TrabajadorDAO();
+		daoTrabajadores.save(tr); //Se añade el objeto a su respectivo Modelo.
+		List<Trabajador> listaTrabajadores = daoTrabajadores.getAll(); //Aca se trae la lista.
+		
+		request.setAttribute("listaTrabajadores", listaTrabajadores);
+		request.getRequestDispatcher("vista_trabajador.jsp").forward(request, response); //Aca se utiliza la interfaz RequestDispatcher para indicar a donde enviar los datos y el método forward para enviar los datos.
 	}
 
 	/**
--- src/cl/inacap/model/Trabajador.java
+++ src/cl/inacap/model/Trabajador.java
@@ -12,42 +12,55 @@ public class Trabajador {
 	public String getExperiencia() {
 		return experiencia;
 	}
+
 	public void setExperiencia(String experiencia) {
 		this.experiencia = experiencia;
 	}
+
 	public String getComentario() {
 		return comentario;
 	}
+
 	public void setComentario(String comentario) {
 		this.comentario = comentario;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
 	public String getPais() {
 		return pais;
 	}
+
 	public void setPais(String pais) {
 		this.pais = pais;
 	}
+
 	public String getTecnologia() {
 		return tecnologia;
 	}
+	
 	public void setTecnologia(String teconologia) {
 		this.tecnologia = teconologia;
 	}
